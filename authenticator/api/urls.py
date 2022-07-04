3c0350bb81c94d9f17ec2cb590acd42c21db3f0c
Neal Waga <waganeal@gmail.com>
2022-07-04 13:34:51 +0300

    url routing

---
+++ authenticator/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+from . import views 
+
+
+urlpatterns = [
+    path ('login', jwt_views.TokenObtainPairView.as_view(), name='login'),
+    
+    path ('run', views.HelloWorld.as_view(), name='hello-world'),
+    path ('me', views.Extractor.as_view(), name='extract-token'),
+]
